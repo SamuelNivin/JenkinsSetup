c52e0c866249560632a266a3f3eef60b05cba85b
Nivin Samuel <nivin@browserstack.com>
2025-07-10 14:59:55 +0530

    Update sample-local.test.js

--- src/sample-local.test.js
+++ src/sample-local.test.js
@@ -1,4 +1,4 @@
-const { Builder, By, until, Capabilities } = require("selenium-webdriver");
+const { Builder, By, Capabilities } = require("selenium-webdriver");
 
 describe("BStack demo test", () => {
   let driver;
@@ -9,17 +9,14 @@ describe("BStack demo test", () => {
       .withCapabilities(Capabilities.chrome())
       .build();
   });
-
+  
   afterAll(async () => {
     await driver.quit();
-  });
+  })
 
   test("local test", async () => {
     await driver.get("http://bs-local.com:45454/");
-    await driver.wait(until.elementLocated(By.tagName("h1")), 10000);
-    const heading = await driver.findElement(By.tagName("h1")).getText();
 
-    console.log("💬 Heading text:", heading);
-    expect(heading).toContain("BrowserStack Local");
-  }, 30000);
+    expect(await driver.getTitle()).toContain('BrowserStack Local');
+  }, 10000000);
 });
